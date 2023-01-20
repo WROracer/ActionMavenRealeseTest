05b32af26a8e3fb5474224a3d453f875a19db90e
WROracer <67609673+WROracer@users.noreply.github.com>
2023-01-20 20:44:02 +0100

    added Read me

--- src/main/java/de/wroracer/Main.java
+++ src/main/java/de/wroracer/Main.java
@@ -1,4 +1,6 @@
-package de.wroracer;public class Main {
+package de.wroracer;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
